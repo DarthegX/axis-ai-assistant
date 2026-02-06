2a40197ac4aac92b4d13f5a4c7d1d364d3dcb735
DarthegX <egarrigosc@gmail.com>
2026-02-07 00:14:39 +0100

    Add signup route to create user

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,31 @@
+import dbConnect from "@/src/lib/db/mongodb";
+import User from "@/src/models/Uset";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const userData = await request.json();
+
+    if (!userData.email || !userData.username || !userData.password) {
+        return NextResponse.json({ error: 'User data incomplete', status: 500 })
+    }
+
+    try {
+        await dbConnect();
+    } catch (err) {
+        console.log('ERROR on POST /auth/signup (connecting database): ' + err)
+        return NextResponse.json({ error: 'Cannot create user. Please try later.', status: 500 })
+    }
+
+    try {
+        const user = await User.insertOne(userData);
+
+        return NextResponse.json({
+            username: user.username,
+            email: user.email,
+            discogs: user.discogs
+        });
+    } catch (err) {
+        console.log('ERROR on POST /auth/signup (creating user): ' + err)
+        return NextResponse.json({ error: 'Cannot create user. Please try later.', status: 500 })
+    }
+}
