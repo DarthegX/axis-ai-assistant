e349db92d83f46c92d28947832ecf51ff4bf3d64
DarthegX <egarrigosc@gmail.com>
2026-02-07 00:13:49 +0100

    Modified texts on init page

--- src/app/init/page.tsx
+++ src/app/init/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
 
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <div className="flex flex-row. items-center">
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-12 justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <div className="flex flex-row. items-center gap-8">
                     <Image
                         className=""
                         src="/axis-logo/thin-96.png"
@@ -35,12 +35,12 @@ export default function Home() {
                 </div>
 
 
-                <div className="pl-2.5 flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                <div className="pl-2.5 flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                        To get started, sign in on Discogs.
+                        Dónde quieres comprar música?
                     </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                        Work in Progress: More platforms will be added soon.
+                        Identifícate con una de estas plataformas.
                     </p>
                 </div>
                 <div className="pl-2 flex flex-col gap-4 text-base font-medium sm:flex-row">
