b615bdd93a34836706619673bedc789956b93a37
DarthegX <egarrigosc@gmail.com>
2026-02-06 23:26:23 +0100

    Add user model

--- src/models/Uset.ts
+++ src/models/Uset.ts
@@ -3,9 +3,11 @@ import mongoose, { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema({
   email: { type: String, unique: true, required: true },
   username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   discogs: {
     accessToken: String,
     accessTokenSecret: String,
+    oauthTokenSecret: String
   },
   spotify: {
     accessToken: String,
