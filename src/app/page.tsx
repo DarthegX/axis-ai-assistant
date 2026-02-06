1722fcf1eea038026465f76e3b1201736b95426c
DarthegX <egarrigosc@gmail.com>
2026-02-06 02:49:54 +0100

    Main page styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,105 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { randomInt } from "node:crypto";
+import { useState } from "react";
+
+
 
 export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const requestDiscogsToken = async () => {
+    const timestamp = Date.now();
+
+    const newTab = window.open("", "_blank");
+
+    try {
+      const discogsOauthResponse = await fetch('/api/discogs/auth', {
+        method: 'GET'
+      });
+      const discogsAuthRedirectUrl = (await discogsOauthResponse.json()).redirectUrl;
+
+      if (newTab) {
+        newTab.location.href = discogsAuthRedirectUrl
+      }
+
+    } catch (err) {
+      newTab?.close();
+    }
+  }
+
+  const getDiscogsAccess = async () => {
+    const timestamp = Date.now();
+
+    try {
+      const discogsOauthResponse = await fetch('/api/discogs/access', {
+        method: 'GET'
+      });
+      const discogsAuthRedirectUrl = (await discogsOauthResponse.json());
+
+      console.log(discogsAuthRedirectUrl)
+
+    } catch (err) {
+      return;
+    }
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <div className="flex flex-row. items-center">
+          <Image
+            className=""
+            src="/axis-logo/thin-96.png"
+            alt="Axis logo"
+            width={100}
+            height={20}
+            priority
+          />
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-white dark:text-zinc-5">Welcome to Axis   </h1>
+        </div>
+
+
+        <div className="pl-2.5 flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, sign in on Discogs.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Work in Progress: More platforms will be added soon.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+        <div className="pl-2 flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <button
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
             rel="noopener noreferrer"
+            onClick={requestDiscogsToken}
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              className=""
+              src="/discogs-logo.webp"
+              alt="Discogs logomark"
+              width={20}
+              height={20}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            Sign in
+          </button>
+
+          <button
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             rel="noopener noreferrer"
+            onClick={getDiscogsAccess}
           >
-            Documentation
-          </a>
+            <Image
+              className=""
+              src="/axis-logo/black-48.png"
+              alt="Discogs logomark"
+              width={20}
+              height={20}
+            />
+            Access
+          </button>
         </div>
       </main>
     </div>
