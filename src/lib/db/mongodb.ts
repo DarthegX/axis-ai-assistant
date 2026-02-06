8ee23cd56108282d381d9286c3d66e2fddc0fbdd
DarthegX <egarrigosc@gmail.com>
2026-02-06 22:18:09 +0100

    add mongo db connection using mongoose

---
+++ src/lib/db/mongodb.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error('Cannot connect to database. Mongo URI not found.');
+}
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  
+  return cached.conn;
+}
+
+export default dbConnect;
