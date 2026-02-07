7ee2acaeea3c78f0be5fabc4e3df3b4877e31eba
DarthegX <egarrigosc@gmail.com>
2026-02-07 02:13:13 +0100

    Return token to page

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,5 +1,7 @@
 import dbConnect from "@/src/lib/db/mongodb";
+import { signToken } from "@/src/lib/jwt";
 import User from "@/src/models/User";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -17,12 +19,42 @@ export async function POST(request: NextRequest) {
     }
 
     try {
-        const user = await User.insertOne(userData);
+        const user = await User.findOneAndUpdate(
+            { email: userData.email },
+            {
+                $set: {
+                    username: userData.username,
+                    email: userData.email,
+                    password: userData.password
+                }
+            },
+            { upsert: true, new: true }
+        );
+
+        const authToken = signToken({
+            userId: user._id.toString(),
+            email: user.email
+        });
+
+        const cookieStore = await cookies();
+
+        cookieStore.set('app_auth_token', authToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'lax',
+            path: '/',
+            maxAge: 60 * 60 * 24 * 20 // 20 days
+        });
 
         return NextResponse.json({
             username: user.username,
             email: user.email,
-            discogs: user.discogs
+            sessionToken: authToken,
+            discogs: {
+                accessToken: null,
+                accessTokenSecret: null,
+                oauthTokenSecret: null
+            }
         });
     } catch (err) {
         console.log('ERROR on POST /auth/signup (creating user): ' + err)
